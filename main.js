ba6dd92310ddfcf6063df3ef9c06003a721ddaef
Tayte <tayte.boss@outlook.com>
2018-07-09 20:09:07 +1000

    a

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+    /* Check width on page load*/
+    if ( $(window).width() < 720) {
+     $('.how-article').removeClass('width-29');
+    }
+    else {}
+ });
+
+ console.log("workings")
