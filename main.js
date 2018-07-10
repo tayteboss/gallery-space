d351a7cfe99992b27041dbcc43e94573ba7ff0e3
Tayte <tayte.boss@outlook.com>
2018-07-10 22:55:52 +1000

    final

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-$(document).ready( function() {
-    /* Check width on page load*/
-    if ( $(window).width() < 720) {
-     $('.how-article').removeClass('width-29');
-    }
-    else {}
- });
+console.log("workings")
 
- console.log("workings")
+$(document).ready(fadeIn)
+$('.how-arrow').hover(arrowHover)
+$('article').hover(articleHover)
+$('#logo').hover(showAbout)
+
+function fadeIn() {
+    console.log('fadein')
+    $('.animate').addClass('make-visible')
+
+}
+
+function arrowHover() {
+    console.log('hover')
+    $(this).toggleClass('how-arrow-a')
+}
+
+function articleHover() {
+    console.log('articlehover')
+    $('article').removeClass('delay-1000 delay-1100 delay-1200 delay-1300 delay-1400')
+    $('article').toggleClass('article-hover article-fade')
+    $(this).toggleClass('article-hover article-fade')
+}
+
+function showAbout() {
+    console.log('show')
+    $('#about').toggleClass('show-about')
+}
