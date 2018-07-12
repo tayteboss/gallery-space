fc6d79c76c1aed40365ec8a9ccdb04382a6a5e69
Tayte <tayte.boss@outlook.com>
2018-07-12 21:54:09 +1000

    a

--- main.js
+++ main.js
@@ -6,24 +6,21 @@ $('article').hover(articleHover)
 $('#logo').hover(showAbout)
 
 function fadeIn() {
-    console.log('fadein')
     $('.animate').addClass('make-visible')
+    $('.hr').addClass('make-width')
 
 }
 
 function arrowHover() {
-    console.log('hover')
     $(this).toggleClass('how-arrow-a')
 }
 
 function articleHover() {
-    console.log('articlehover')
     $('article').removeClass('delay-1000 delay-1100 delay-1200 delay-1300 delay-1400')
     $('article').toggleClass('article-hover article-fade')
     $(this).toggleClass('article-hover article-fade')
 }
 
 function showAbout() {
-    console.log('show')
     $('#about').toggleClass('show-about')
 }
